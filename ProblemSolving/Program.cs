1d895df1ccf6f6e302e446bc563f752aafb494f1
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-07-25 13:29:34 +0100

    Add project files.

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProblemSolving
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //write a method that checks if each word in the string starts with lower case and if so, print true.
+            //Fadekemi is a great C# developer--- returns false
+           //fadekemi will build face recognition library in one week  ---returns true(it has to be true, literally)
+
+        }
+    }
+}
