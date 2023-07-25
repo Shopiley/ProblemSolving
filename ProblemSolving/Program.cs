b544345d4413c9893cae41455e6d37e305d34a0d
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-07-25 16:12:31 +0100

    Sort array

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -21,6 +21,7 @@ namespace ProblemSolving
             return true;
         }
 
+
         static string DoubleHashSubstring(string str)
         {
             string result = "";
@@ -32,21 +33,36 @@ namespace ProblemSolving
                 if (str[l] == '#' && str[l+1] == '#')
                 {
                     r = l+2;
-
                     while (str[r] != '#')
                     {
                         result += str[r];
                         r++;
                     }
                     break;
-
                 }
                 l++;
             }
             return result;
-
         }
 
+        static int[] SortArray(int[] array)
+        {
+            // I implemented the bubble sort algorithm for this
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n-i-1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }          
+            }
+            return array;
+        }
         static void Main(string[] args)
         {
             //write a method that checks if each word in the string starts with lower case and if so, print true.
@@ -57,12 +73,19 @@ namespace ProblemSolving
 
 
             // write a method that returns substring from between two double hash signs (#)
-            string word1 = "##abc##def"; 
-            string word2 = "##1278##"; 
-            Console.WriteLine(DoubleHashSubstring(word1));  // returns abc
-            Console.WriteLine(DoubleHashSubstring(word2));  // returns 1278
+            string word1 = "##abc##def"; // returns abc
+            string word2 = "##1278##"; // returns 1278
+            Console.WriteLine(DoubleHashSubstring(word1));
+            Console.WriteLine(DoubleHashSubstring(word2));
 
-
+            // write a method that returns sorted array in ascending order.
+            int[] nums = { 9, 5, 7, 2, 1, 8 }; // gives {1, 2, 5, 7, 8, 9}
+            Console.Write("{ ");
+            foreach (var num in SortArray(nums))
+            {
+                Console.Write(num + ", ");
+            }
+            Console.Write("}");
         }
     }
 }
